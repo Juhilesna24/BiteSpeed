4f346bf77c8387f2889d983a6b73070d930e14a3
Juhilesna24 <jlesna66@gmail.com>
2023-08-23 11:11:52 +0530

    remove build file

--- dist/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbConfig = void 0;
-exports.dbConfig = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'bitespeed',
-};
